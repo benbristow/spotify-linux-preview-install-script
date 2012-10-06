fc8459716b75bfabc6d41350dc86bcc1e4d3f4df
Ben Bristow <benbristow@outlook.com>
2012-10-06 20:14:27 +0100

    Remove auto launch Spotify and cleanup scrpt

--- spotifyinstall.sh
+++ spotifyinstall.sh
@@ -11,21 +11,14 @@ echo "> Adding Repo"
 sudo echo "##Spotify (Automatically added by SLIS)" >> /etc/apt/sources.list
 sudo echo "deb http://repository.spotify.com stable non-free" >> /etc/apt/sources.list
 clear
-echo "> Adding Repo"
 echo "> Adding Key"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
 clear
-echo "> Adding Repo"
-echo "> Adding Key"
 echo "> Updating Package List"
 sudo apt-get update
 clear
-echo "> Adding Repo"
-echo "> Adding Key"
-echo "> Updating Package List"
 echo "> Installing Spotify!"
 sudo apt-get install spotify-client
 clear
-echo "> Launching Spotfiy"
-spotify &
 echo "Done!"
+echo "If all went well then you should be able to launch Spotify via the Unity Dash"
