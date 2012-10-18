e419c5f548b9f480cb8b936465e9ae6e5a99bfbe
Ben Bristow <benbristow@outlook.com>
2012-10-18 18:51:15 +0200

    Fix root authentication

--- spotifyinstall.sh
+++ spotifyinstall.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
 if [ $EUID != 0 ]; then
     sudo "$0" "$@"
     exit $?
+fi
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
 fi
 
 echo "Spotify Linux Install Script for Ubuntu 12.04+"
@@ -18,7 +24,7 @@ echo "> Updating Package List"
 sudo apt-get update
 clear
 echo "> Installing Spotify!"
-sudo apt-get install spotify-client
+sudo apt-get install -y spotify-client
 clear
 echo "Done!"
 echo "If all went well then you should be able to launch Spotify via the Unity Dash"
