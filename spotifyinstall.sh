003ea51ced7157717c28bee39e1d59e818af7b31
joar <joar@wandborg.se>
2013-02-06 15:11:05 +0100

    Update spotifyinstall.sh
    Removed superfluous `sudo` uses.
    
    Since the script automatically runs itself as root via sudo and exits with an error if it isn't run as root, the sudo statements in the spotify installation part of the script are unnecessary.

--- spotifyinstall.sh
+++ spotifyinstall.sh
@@ -14,17 +14,17 @@ echo "By Ben Bristow | Licensed Under the GNU GPLv3"
 echo "Available on GitHub"
 echo "==============================================="
 echo "> Adding Repo"
-sudo echo "##Spotify (Automatically added by SLIS)" >> /etc/apt/sources.list
-sudo echo "deb http://repository.spotify.com stable non-free" >> /etc/apt/sources.list
+echo "## Spotify (Automatically added by SLIS)" >> /etc/apt/sources.list
+echo "deb http://repository.spotify.com stable non-free" >> /etc/apt/sources.list
 clear
 echo "> Adding Key"
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
 clear
 echo "> Updating Package List"
-sudo apt-get update
+apt-get update
 clear
 echo "> Installing Spotify!"
-sudo apt-get install -y spotify-client
+apt-get install -y spotify-client
 clear
 echo "Done!"
 echo "If all went well then you should be able to launch Spotify via the Unity Dash"
